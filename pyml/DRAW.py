f86bcd9d5528de2580dcde67c6ae202510874a3d
Peachball <chenyexu@gmail.com>
2016-11-08 19:55:22 -0800

    Better draw?

--- pyml/DRAW.py
+++ pyml/DRAW.py
@@ -2,15 +2,22 @@ import theano
 import theano.tensor as T
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 def load_mnist():
     from keras.datasets import mnist
-    return mnist.load_data()
+    (a, _), (c, _) = mnist.load_data()
+    a = a.astype('float32') / 255.0
+    c = c.astype('float32') / 255.0
+    a = a.reshape(-1, 784)
+    c = c.reshape(-1, 784)
+    return a, c
 
-
-def add_fc_layer(name, inp, out_size):
+def add_fc_layer(name, inp, out_size, share=True):
     shape = list(inp.get_shape())
     with tf.variable_scope(name) as scope:
+        if share:
+            scope.reuse_variables()
         w = tf.get_variable('w', [shape[-1], out_size])
         b = tf.get_variable('b', [out_size])
 
@@ -22,65 +29,146 @@ def add_fc_layer(name, inp, out_size):
             return tf.matmul(inp, w) + b
         raise Exception("Not implemented")
 
-def DRAWNetwork():
-    (X_t, _), (X_v, _) = load_mnist()
+def read(x_t, x_hat, dec_h):
+    return tf.concat(1, [x_t, x_hat])
 
-    X = tf.placeholder(tf.float32, [None, 784], name='Input')
+def _get_encoder(hidden_size=256):
+    lstm = tf.nn.rnn_cell.LSTMCell(hidden_size, use_peepholes=True)
+    return tf.nn.rnn_cell.MultiRNNCell([lstm] * 1)
+
+def encode(enc_h, r, dec_h, hidden_size=256, init_state=None, batch_size=32,
+        share=True):
+    with tf.variable_scope("encoder") as scope:
+        if share:
+            scope.reuse_variables()
+        encoder = _get_encoder(hidden_size)
+        if init_state is None:
+            init_state = encoder.zero_state(batch_size, tf.float32)
+
+        h_enc, state = encoder(tf.concat(1, [r, dec_h]), init_state)
+
+    return h_enc, state
+
+def _get_encoder_state(hidden_size=256, batch_size=32):
+    with tf.variable_scope("encoder") as scope:
+        encoder = _get_encoder(hidden_size)
+        init_state = encoder.zero_state(batch_size, tf.float32)
+        return init_state
+
+def _get_decoder(hidden_size=256):
+    lstm = tf.nn.rnn_cell.LSTMCell(hidden_size, use_peepholes=True)
+    return tf.nn.rnn_cell.MultiRNNCell([lstm] * 1)
+
+def decode(dec_h, z, hidden_size=256, init_state=None, batch_size=32, share=True):
+    with tf.variable_scope("decoder") as scope:
+        if share:
+            scope.reuse_variables()
+        decoder = _get_decoder(hidden_size)
+        if init_state is None:
+            init_state = decoder.zero_state(batch_size, tf.float32)
+
+        dec_h, state = decoder(z, init_state)
+    return dec_h, state
+
+def _get_decoder_state(hidden_size=256, batch_size=32):
+    with tf.variable_scope("decoder") as scope:
+        decoder = _get_decoder()
+        init_state = decoder.zero_state(batch_size, tf.float32)
+        return init_state
+
+def sample(enc_h, latent_dim=300, share=True, latent_loss=None):
+    l = add_fc_layer("sampler", enc_h, latent_dim * 2, share=share)
+    z_mean, z_log_var = tf.split(1, 2, l)
+    z = tf.random_normal(z_mean.get_shape()) * tf.exp(z_log_var / 2.0) +\
+            z_mean
+    if latent_loss is not None:
+        latent_loss += 0.5 * tf.reduce_mean(tf.square(z_mean) +
+                tf.exp(z_log_var) -
+                z_log_var + 1)
+    return z, latent_loss
+
+def write(dec_h, out_size=784, share=True):
+    l = add_fc_layer("write", dec_h, out_size, share=share)
+    return l
+
+def DRAWNetwork():
     batch_size = 32
     timesteps = 10
-    latent_size = 300
+    latent_size = 10
+    logdir = 'tflogs/draw/'
 
-    x_bat = tf.placeholder(tf.float32, [batch_size, 784], name='batch_inp')
+    X_t, X_v = load_mnist()
+    from tensorflow.examples.tutorials import mnist
+    X_b = mnist.input_data.read_data_sets('/tmp/mnist', one_hot=True).train
 
-    with tf.variable_scope("encoder") as scope:
-        lstm = tf.nn.rnn_cell.LSTMCell(500, use_peepholes=True)
-        enc_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm] * 3)
-        enc_state = enc_lstm.zero_state(batch_size, tf.float32)
-        e_state = enc_state
+    X = tf.placeholder(tf.float32, [batch_size, 784], name='Input')
 
-    with tf.variable_scope("decoder") as scope:
-        lstm = tf.nn.rnn_cell.LSTMCell(500, use_peepholes=True)
-        dec_lstm = tf.nn.rnn_cell.MultiRNNCell([lstm] * 3)
-        dec_state = dec_lstm.zero_state(batch_size, tf.float32)
-        d_state = dec_state
+    init_canvas = canvas = tf.constant(0.0, tf.float32, [batch_size, 784])
+    init_h_enc_state = h_enc_state = _get_encoder_state()
+    init_h_dec_state = h_dec_state = _get_decoder_state()
+    init_h_enc = h_enc = tf.constant(0.0, tf.float32, [batch_size, 256])
+    init_h_dec = h_dec = tf.constant(0.0, tf.float32, [batch_size, 256])
 
-    init_canvas = tf.constant(0.0, dtype=tf.float32, shape=[batch_size, 784], name='canvas')
-    init_h_dec = tf.constant(0.0, tf.float32, [batch_size, 784], name='decoded')
-    init_h_enc = tf.constant(0.0, tf.float32, [batch_size, 300], name='encoded')
-
-    canvas = init_canvas
-    h_dec = init_h_dec
-    h_enc = init_h_enc
+    share = False
+    drawings = [canvas]
+    latent_loss = 0.0
 
     for i in range(timesteps):
-        x_hat = x_bat - tf.sigmoid(canvas)
-        r_t = tf.concat(1, [X, x_hat, h_dec])
-        with tf.variable_scope("encoder") as scope:
-            output, e_state = enc_lstm(r_t, e_state)
-            scope.reuse_variables()
-
-        vals = add_fc_layer('fc', output, 2 * latent_size)
-        z_mean, z_log_var = tf.split(1, 2, vals)
-        print(z_mean.get_shape())
-
-        z = tf.random_normal(z_mean.get_shape()) * tf.exp(z_log_var / 2.0)\
-                + z_mean
-
-        with tf.variable_scope("decoder") as scope:
-            h_dec = dec_lstm(tf.concat(1, [z, h_dec]), d_state)
-            scope.reuse_variables()
-        canvas = canvas + h_dec
-
-        tf.get_variable_scope().reuse_variables()
-
+        x_hat = X - tf.sigmoid(canvas)
+        r_t = read(X, x_hat, h_dec)
+        h_enc, h_enc_state = encode(h_enc, r_t, h_dec, init_state=h_enc_state,
+                share=share)
+        z, latent_loss = sample(h_enc, share=share, latent_loss=latent_loss,
+                latent_dim=latent_size)
+        h_dec, h_dec_state = decode(h_dec, z, init_state=h_dec_state,
+                share=share)
+        canvas = canvas + write(h_dec, share=share)
+        drawings += [tf.sigmoid(canvas)]
+        share = True
 
     final_canvas = canvas
-    final_state = [e_state, d_state]
+    final_enc_state = h_enc
+    final_dec_state = h_dec
+
+    Lx = tf.reduce_mean(tf.reduce_sum(tf.squared_difference(tf.sigmoid(canvas),
+        X), 1))
+
+    loss = Lx + latent_loss
+    tf.scalar_summary("Loss", loss)
+
+    summaries = tf.merge_all_summaries()
+    saver = tf.train.Saver()
+    opt = tf.train.AdamOptimizer(0.001, beta1=0.5)
+    grad = opt.compute_gradients(loss)
+    for i, (g, v) in enumerate(grad):
+        if g is not None:
+            grad[i] = (tf.clip_by_norm(g, 5), v)
+
+    train_step = opt.apply_gradients(grad)
 
     init_op = tf.initialize_all_variables()
 
     with tf.Session() as sess:
+        plt.ion()
+        sw = tf.train.SummaryWriter(logdir, sess.graph)
         sess.run(init_op)
+        iteration = 0
+        for i in range(1000):
+            print(X_b.next_batch(batch_size)[0].shape)
+            s, _, e, d, lat = sess.run([summaries, train_step, loss, drawings,
+                latent_loss],
+                    feed_dict={X: X_b.next_batch(batch_size)[0]})
+            print("Lx: {} Latent: {}".format(e, lat))
+            sw.add_summary(s)
+            iteration += 1
+            if iteration % 100 == 0:
+                saver.save(sess, logdir + 'model', global_step=iteration)
+
+            plt.subplot(121)
+            plt.imshow(d[10][0].reshape(28, 28), cmap='Greys')
+            plt.subplot(122)
+            plt.imshow(X_t[i].reshape(28, 28), cmap='Greys')
+            plt.pause(0.05)
 
 if __name__ == '__main__':
     DRAWNetwork()
--- pyml/supervised.py
+++ pyml/supervised.py
@@ -3,6 +3,219 @@ import theano
 import numpy as np
 import matplotlib.pyplot as plt
 
+def generateAdagrad(params, error, alpha=0.01, epsilon=1e-8, verbose=False,
+        clip=None):
+    updates = []
+    history = []
+
+    if verbose:
+        print("Calculating gradients")
+    gradients = T.grad(error, params)
+    if verbose:
+        print("Done with gradients")
+    count = 0
+    for p, grad in zip(params, gradients):
+        shape = p.get_value().shape
+
+        totalG = theano.shared(value=np.zeros(shape).astype(theano.config.floatX))
+
+        new_g = totalG + T.sqr(grad)
+        updates.append((totalG, new_g))
+        deltaw = grad / (T.sqrt(new_g) + epsilon) * alpha
+        if isinstance(clip, tuple):
+            deltaw = T.clip(deltaw, clip[0], clip[1])
+        updates.append((p, p - deltaw))
+
+        history.append(totalG)
+
+    if verbose: print('')
+    return (history, updates)
+
+def generateAdadelta(params, error, decay=0.9, alpha=1, epsilon=1e-8):
+    updates = []
+    accUpdates = []
+    accGrad = []
+
+    gradients = T.grad(error, params)
+    for p, grad in zip(params, gradients):
+        shape = p.get_value().shape
+
+        Eg = theano.shared(value=np.zeros(shape).astype(theano.config.floatX))
+        Ex = theano.shared(value=np.zeros(shape).astype(theano.config.floatX))
+
+        new_g = decay * Eg + (1 - decay) * T.sqr(grad)
+
+        d_x = T.sqrt((Ex + epsilon) / (new_g + epsilon)) * grad * alpha
+        new_x = decay * Ex + (1 - decay) * T.sqr(d_x)
+
+        updates.append((p, p - d_x))
+        updates.append((Ex, new_x))
+        updates.append((Eg, new_g))
+
+        accUpdates.append(Ex)
+        accGrad.append(Eg)
+
+    return ([accUpdates, accGrad], updates)
+
+def generateAdam(params, error, alpha=0.001, decay1=0.9, decay2=0.999,
+        epsilon=1e-8, verbose=False):
+    """
+        Generate updates for the adam type of stochastic gradient descent
+
+            Variable interp.
+    """
+    updates = []
+    moment = []
+    vector = []
+
+    if type(alpha) == 'float':
+        alpha = theano.shared(np.array(alpha).astype(theano.config.floatX))
+    time = theano.shared(np.array(1.0).astype(theano.config.floatX))
+    epsilon = theano.shared(np.array(epsilon).astype(theano.config.floatX))
+    updates.append((time, time+1))
+    i = 0
+    gradients = T.grad(error, params)
+    decay1 = theano.shared(np.array(decay1).astype(theano.config.floatX))
+    decay2 = theano.shared(np.array(decay2).astype(theano.config.floatX))
+    for p, grad in zip(params, gradients):
+        shape = p.get_value().shape
+        grad = T.grad(error, p)
+
+        m = theano.shared(value=np.zeros(shape).astype(theano.config.floatX))
+        v = theano.shared(value=np.zeros(shape).astype(theano.config.floatX))
+
+        m_t = decay1 * m + (1 - decay1) * grad
+        v_t = decay2 * v + (1 - decay2) * T.sqr(grad)
+        m_adj = m_t / (1.0 - T.pow(decay1, time))
+        v_adj = v_t / (1.0 - T.pow(decay2, time))
+
+        updates.append((m, m_t))
+        updates.append((v, v_t))
+        # updates.append((p, p - alpha * m_adj / (T.sqrt(v_adj) + epsilon)))
+
+        moment.append(m)
+        vector.append(v)
+        if verbose: print("\rDone with {}/{}".format(i+1, len(params)), end="")
+        i += 1
+
+    if verbose: print("")
+    return (moment + vector + [time, alpha, epsilon], updates)
+
+def generateRmsProp(
+        params, error, alpha=0.01, decay=0.9, fudge=1e-3, verbose=False):
+    r = []
+    v = []
+    pr = []
+    updates = []
+    alpha = theano.shared(np.array(alpha).astype(theano.config.floatX))
+    count = 0
+    for p in params:
+        grad = T.grad(error, p)
+
+        shape = p.get_value().shape
+        r_t = theano.shared(np.zeros(shape).astype(theano.config.floatX))
+        v_t = theano.shared(np.zeros(shape).astype(theano.config.floatX))
+
+        new_r = (1 - decay) * T.sqr(grad) + decay * r_t
+        new_v = alpha / (T.sqrt(new_r) + fudge) * grad
+        updates.append((r_t, new_r))
+        updates.append((v_t, new_v))
+        updates.append((p, p - new_v))
+        r.append(r_t)
+        v.append(v_t)
+
+        count += 1
+        if verbose: print("\rGradient {}/{} done".format(count, len(params)),
+                end="")
+
+    if verbose: print('')
+    return (r + v, updates)
+
+def generateVanillaUpdates(params, error, alpha=0.01, verbose=True):
+    grad = []
+    count = 0
+    for p in params:
+        grad.append(T.grad(error, p))
+        count += 1
+        print("\r{}/{} Gradients done".format(count, len(params)), end="")
+    updates = [(p, p - g * alpha) for p, g in zip(params, grad)]
+    print("")
+    return updates
+
+def generateMomentumUpdates(params, error, alpha=0.01, momentum=0.5):
+    grad = []
+    if type(alpha) == float:
+        alpha = theano.shared(np.array(alpha).astype(theano.config.floatX))
+    if type(momentum) == float:
+        momentum = theano.shared(np.array(momentum)
+                                    .astype(theano.config.floatX))
+    for p in params:
+        grad.append(T.grad(error, p))
+    mparams = [theano.shared(np.zeros(p.shape.eval()).astype(theano.config.floatX)) for p in params]
+    gradUpdates = [(p, p - g) for p, g in zip(params,mparams)]
+
+    gradUpdates += [(m, momentum * m + alpha * g) for m, g in
+        zip(mparams, grad)]
+    return ([gradUpdates, mparams], gradUpdates)
+
+def generateNesterovMomentumUpdates(params, error, alpha=0.01, momentum=0.9,
+        decay=1e-6):
+    print("WARNING: NOT FULLY IMPLEMENTED YET")
+    updates = []
+    momentum = []
+    for p in params:
+        v_t = theano.shared(np.zeros(p.get_value().shape)
+                .astype(theano.config.floatX))
+        grad = T.grad(error, p + momentum * v_t)
+
+        updates.append((p, p + v_t))
+        updates.append((v_t, momentum * v_t - alpha * grad))
+
+        momentum.append(v_t)
+
+    return (momentum, updates)
+
+def generateRpropUpdates(params, error, init_size=1, verbose=False):
+    prevw = []
+    deltaw = []
+    updates = []
+    gradients = []
+    #initalize stuff
+    for p in params:
+        prevw.append(theano.shared(np.zeros(p.shape.eval()).astype(config.floatX)))
+        deltaw.append(theano.shared(init_size * np.ones(p.shape.eval()).
+            astype(config.floatX)))
+
+    iterations = 0
+    for p, dw, pw in zip(params, deltaw, prevw):
+        try:
+            if verbose: print("\rGradient {} out of {}".format(iterations + 1, len(params)), end="")
+            gradients.append(T.grad(error, p))
+            iterations += 1
+        except Exception:
+            print('Unused input')
+            continue
+        #Array describing which values are when gradients are both positive or both negative
+        simW = T.neq((T.eq((pw > 0), (gradients[-1] > 0))), (T.eq((pw < 0), (gradients[-1] <
+            0))))
+
+        #Array describing which values are when gradients are in opposite directions
+        diffW = ((pw > 0) ^ (gradients[-1] > 0)) * (T.neq(pw, 0) * T.neq(gradients[-1], 0))
+        updates.append((p, p - (T.sgn(gradients[-1]) * dw * (T.eq(diffW, 0)))))
+        updates.append((dw, T.switch(diffW, dw *
+            0.5, T.switch(simW, dw * 1.2, dw))))
+        updates.append((pw, (T.sgn(gradients[-1]) * dw * (T.eq(diffW, 0)))))
+
+    storage = prevw + deltaw
+    if verbose: print("\nDone with updates")
+
+    return (storage, updates)
+
+def getRegularization(params):
+    reg = T.sum(T.sqr(params[0]))
+    for p in params[1:]:
+        reg = reg + T.sum(T.sqr(p))
+    return reg
 
 def get_weight(shape, scale=0.05, name=None):
     return theano.shared(
@@ -284,5 +497,166 @@ def rtrl2():
     np.savetxt('jacobian.csv', z)
 
 
+def rtrl3():
+    """Multilayer rtrl gru version"""
+    INPUT_SIZE = 1
+    LAYERS = (10,)
+    OUTPUT_SIZE = 1
+    lr = theano.shared(np.array(1e-3).astype(theano.config.floatX))
+
+    X = T.vector()
+    Y = T.vector()
+
+    params = []
+    stor_grad_updates = []
+    grad_updates = []
+    hid_updates = []
+    def add_layer(x, size):
+        l_data = {}
+        dim_theta = 3 * size[0] * size[1] + 3 * size[1] * size[1]
+        theta = get_weight((dim_theta,))
+
+        index = 0
+        W = theta[index:(index + 3 * size[0] * size[1])].reshape(
+                (3 * size[1], size[0]))
+        index += size[0] * size[1]
+
+        U = theta[index:(index + 3 * size[1] * size[1])].reshape(
+                (3 * size[1], size[1]))
+        index += 3 * size[1] * size[1]
+
+        bias = theta[index:(index + 3 * size[1])] # Gate, Hidden, Reset
+
+        h_tm1 = get_weight((size[1],))
+
+        wx = T.dot(W, x)
+        hh = T.dot(U, h_tm1)
+
+        z = T.nnet.sigmoid(wx[:size[1]] + hh[:size[1]] + bias[:size[1]])
+        r = T.nnet.sigmoid(wx[size[1]:(2*size[1])]
+                + hh[size[1]:(2*size[1])]
+                + bias[size[1]:(2*size[1])])
+        net_h = T.tanh(wx[-size[1]:] + hh[-size[1]:] + bias[-size[1]:])
+        h_t = (1 - z) * h_tm1 + z * net_h
+
+        weight_gradient = theano.shared(
+                np.zeros((size[1], dim_theta)).astype(theano.config.floatX)
+                )
+        hidden_gradient = theano.shared(
+                np.zeros((size[1], size[0])).astype(theano.config.floatX)
+                )
+
+        grad_upd = []
+        hh_grad = theano.gradient.jacobian(h_t, h_tm1)
+        xh_grad = theano.gradient.jacobian(h_t, theta)
+        grad_upd.append(
+                (weight_gradient, T.dot(hh_grad, weight_gradient) + xh_grad))
+        grad_upd.append(
+                (hidden_gradient, T.dot(hh_grad, hidden_gradient) +
+                    theano.gradient.jacobian(h_t, x)))
+
+        def reset():
+            weight_gradient.set_value(
+                    np.zeros((size[1], dim_theta)).astype(theano.config.floatX))
+            hidden_gradient.set_value(
+                    np.zeros((size[1], size[0])).astype(theano.config.floatX))
+            h_tm1.set_value(
+                    np.zeros((size[1],)).astype(theano.config.floatX))
+
+        l_data['h_tm1'] = h_tm1
+        l_data['param'] = theta
+        l_data['reset'] = reset
+        l_data['hh_grad'] = hidden_gradient
+        l_data['param_grad'] = weight_gradient
+        l_data['grad_upd'] = grad_upd
+        l_data['hid_updates'] = [(h_tm1, h_t)]
+
+        return h_t, l_data
+
+    layers = []
+    layers.append(add_layer(X, (INPUT_SIZE, LAYERS[0])))
+    for i in range(len(LAYERS)):
+        if i >= len(LAYERS) - 1:
+            break
+        l = layers[-1][0]
+        layers.append(add_layer(l, (LAYERS[i], LAYERS[i+1])))
+    w_ho = get_weight((OUTPUT_SIZE, LAYERS[-1]))
+    b_o = get_weight((OUTPUT_SIZE))
+    l = layers[-1][0]
+    output = T.dot(w_ho, l) + b_o
+
+    J = T.mean(T.sqr(output - Y))
+    grad_updates += [(w_ho, w_ho - lr * T.grad(J, w_ho)),
+                     (b_o, b_o - lr * T.grad(J, b_o))]
+    dJdh = T.grad(J, l).T
+
+    def reset():
+        for l in layers:
+            l[1]['reset']()
+
+    def get_value(v):
+        return (theano.function(
+                [X, Y],
+                v,
+                # updates=(stor_grad_updates + hid_updates + grad_updates),
+                allow_input_downcast=True,
+                on_unused_input='ignore')(np.arange(INPUT_SIZE).astype(theano.config.floatX),
+                    np.arange(OUTPUT_SIZE).astype(theano.config.floatX))
+                )
+
+    def print_value(v):
+        print(get_value(v))
+
+    for l in layers[::-1]:
+        stor_grad_updates += l[1]['grad_upd']
+        hid_updates += l[1]['hid_updates']
+        grad_updates += [
+                (l[1]['param'], l[1]['param']
+                    - lr * T.dot(dJdh, l[1]['param_grad']).T)]
+        dJdh = T.dot(dJdh, l[1]['hh_grad'])
+
+    predict = theano.function([X], output, allow_input_downcast=True)
+    learn = theano.function(
+        [X, Y],
+        J,
+        updates=(stor_grad_updates + hid_updates + grad_updates),
+        allow_input_downcast=True)
+
+
+    #Train testing
+    x = np.linspace(0, 10, 100).astype('float32')[:, None]
+    y = np.sin(x)
+
+    train_error = []
+    plt.ion()
+    for i in range(10000):
+        reset()
+        tot_err = 0
+        for j in range(x.shape[0]):
+            e  = (learn(x[j], y[j]))
+            tot_err += e
+        avg_err = 1.0 * tot_err / x.shape[0]
+        print(avg_err)
+        if i > 0 and train_error[-1] < avg_err:
+            lr.set_value(lr.get_value().astype('float32') / 2)
+        train_error.append(avg_err)
+        if i % 100 == 0:
+            plt.ion()
+            plt.figure(0)
+            plt.cla()
+            plt.plot(train_error)
+            plt.pause(0.05)
+
+        if i % 1000 == 0:
+            plt.ioff()
+            out_val = []
+            for j in range(x.shape[0]):
+                out_val.append(predict(x[j]))
+            plt.figure(1)
+            plt.plot(out_val)
+            plt.plot(y)
+            plt.show()
+
+
 if __name__ == '__main__':
-    rtrl2()
+    rtrl3()
