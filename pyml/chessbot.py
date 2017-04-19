a17fe8d79be086e04ee00741ac3488d81d2f6fc3
Peachball <chenyexu@gmail.com>
2017-04-18 21:30:40 -0700

    Add windows compatibility

--- pyml/chessbot.py
+++ pyml/chessbot.py
@@ -8,12 +8,14 @@ import threading
 import time
 import os
 import requests
+import random
+import os
 
 
 def get_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--logdir", default='tflogs/chess/sf',
+    parser.add_argument("--logdir", default='tflogs/chess/temporaldiff',
             type=str, help="Location to store tensorflow logs")
 
     parser.add_argument("--batch_size", default=32, type=int,
@@ -64,8 +66,8 @@ def convert_to_array(board):
     return (white, np.fliplr(black))
 
 
-def play_self(sess, enqueue, X, Y, V, coord, verbose=False, log_dir=None,
-        engine=None):
+def play_self(sess, enqueue, X, Y, V, coord, W, verbose=False, log_dir=None,
+        engine=None, sw=None, thread_id=0):
     SAVE_TIME = 0
     b = chess.Board()
     b.reset()
@@ -81,17 +83,28 @@ def play_self(sess, enqueue, X, Y, V, coord, verbose=False, log_dir=None,
                 ww = 0
                 bw = 0
             if '1-0' == r:
-                ww = 1
-                bw = 0
+                ww = 100
+                bw = -100
             if '0-1' == r:
-                ww = 0
-                bw = 1
+                ww = -100
+                bw = 100
             print("Played game of length {}, {}"
                     .format(len(white_states), r))
-            sess.run(enqueue, feed_dict={X: np.array(white_states),
-                Y: [ww] * len(white_states)})
-            sess.run(enqueue, feed_dict={X: np.array(black_states), Y:[bw] *
-                len(black_states)})
+            w_vals = np.concatenate((
+                    sess.run(V, feed_dict={X: np.array(white_states)})[1:, 0],
+                    np.array(ww)[None]))
+            b_vals = np.concatenate((
+                    sess.run(V, feed_dict={X: np.array(black_states)})[1:, 0],
+                    np.array(bw)[None]))
+            if sw is not None:
+                game_summary = tf.Summary(
+                        value=[tf.Summary.Value(tag="Game length",
+                            simple_value=len(white_states))])
+                sw.add_summary(game_summary)
+            sess.run(enqueue, feed_dict={X: np.array(white_states), Y: w_vals,
+                W: [ww] * len(white_states)})
+            sess.run(enqueue, feed_dict={X: np.array(black_states), Y: b_vals,
+                W: [bw] * len(black_states)})
 
             del white_states[:]
             del black_states[:]
@@ -105,7 +118,48 @@ def play_self(sess, enqueue, X, Y, V, coord, verbose=False, log_dir=None,
                 s_t = time.clock()
             b.reset()
 
+        def minmax(board, depth, white=True, getmove=False, top=5):
+            # param white is "is it whites turn?"
+            if depth == 0:
+                assert(not getmove)
+                wh, bl = convert_to_array(board)
+                if white:
+                    return -sess.run(V, feed_dict={X: bl[None]})
+                else:
+                    return -sess.run(V, feed_dict={X: wh[None]})
+
+            moves = list(board.legal_moves)
+
+            states = []
+            for m in moves:
+                board.push(m)
+                wh, bl = convert_to_array(board)
+                if white:
+                    states += [wh]
+                else:
+                    states += [bl]
+                board.pop()
+            values = sess.run(V, feed_dict={X: np.array(states)})
+            top_n = np.argsort(values, axis=0)[-top:]
+            subscore = -1e8
+            best_move = None
+            for ind in top_n:
+                i = int(ind)
+                board.push(moves[int(ind)])
+                score = -minmax(board, depth - 1, white=not white)
+                board.pop()
+                if score > subscore:
+                    subscore = score
+                    best_move = moves[int(ind)]
+
+            if getmove:
+                return best_move
+            else:
+                return subscore
+
         def select_move(white=True, T=-1, use_engine=False):
+            if T == 'minmax':
+                return minmax(b, 1, white=white, getmove=True, top=2)
             if use_engine and (not engine is None):
                 engine.position(b)
                 return engine.go()[0]
@@ -141,12 +195,18 @@ def play_self(sess, enqueue, X, Y, V, coord, verbose=False, log_dir=None,
             return moves[m_i]
 
         if b.turn:
-            curmove = select_move(b.turn, use_engine=True)
+            if thread_id % 2 == 0:
+                curmove = select_move(b.turn, use_engine=True)
+            else:
+                curmove = select_move(b.turn, "minmax")
             b.push(curmove)
             white, black = convert_to_array(b)
             white_states.append(white)
         else:
-            curmove = select_move(b.turn, None)
+            if thread_id % 2 == 1:
+                curmove = select_move(b.turn, use_engine=True)
+            else:
+                curmove = select_move(b.turn, "minmax")
             b.push(curmove)
             white, black = convert_to_array(b)
             black_states.append(black)
@@ -186,18 +246,20 @@ def build_net(X, histograms=False):
     if histograms:
         tf.summary.histogram('conv_1', net)
 
-    for i in range(3):
-        # with tf.variable_scope("resnet{}".format(i)):
-        # I = net
-        net = add_conv_layer(net, [3, 3], 64, 'conv{}_0'.format(i+2))
-        # net = tf.contrib.layers.batch_norm(net, 0.99)
-        net = tf.nn.elu(net)
-        net = add_conv_layer(net, [3, 3], 64, 'conv{}_1'.format(i+2))
-        # net = tf.contrib.layers.batch_norm(net, 0.99)
+    for i in range(15):
+        with tf.variable_scope("resnet{}".format(i)):
+            I = net
+            net = add_conv_layer(net, [3, 3], 64, 'conv{}_0'.format(i+2))
+            net = tf.contrib.layers.batch_norm(net, 0.99)
+            net = tf.nn.elu(net)
+            if histograms:
+                tf.summary.histogram("conv{}_0".format(i + 2), net)
+            net = add_conv_layer(net, [3, 3], 64, 'conv{}_1'.format(i+2))
+            net = tf.contrib.layers.batch_norm(net, 0.99)
 
-        net = tf.nn.elu(net)
-            # if histograms:
-                # tf.summary.histogram('conv_{}'.format(i+2), net)
+            net = tf.nn.elu(net) + I
+            if histograms:
+                tf.summary.histogram("conv{}_1".format(i + 2), net)
 
     net = flatten(net)
     features = net
@@ -206,21 +268,25 @@ def build_net(X, histograms=False):
         tf.summary.histogram('output_value', net)
     return net
 
+
 def get_model_params(X, L, cpu=0):
     device = '/cpu:{}'.format(cpu)
     with tf.device(device):
         with tf.variable_scope('valuenet', reuse=True):
             return build_net(tf.one_hot(X, 13))
 
+
 def main():
     args = get_args()
     with tf.name_scope('DataManagement'):
-        data_queue = tf.RandomShuffleQueue(60000, 1024, [tf.int64, tf.float32],
-                [[8, 8], 1])
+        data_queue = tf.RandomShuffleQueue(60000, 128, [tf.int64, tf.float32, tf.int32],
+                [[8, 8], 1, 1])
         X = tf.placeholder(tf.int64, [None, 8, 8], name='input')
-        L_d = tf.placeholder(tf.float32, [None], name='win_status')
-        enqueue_op = data_queue.enqueue_many([X, tf.expand_dims(L_d, axis=1)])
-        B, L = data_queue.dequeue_many(args.batch_size)
+        L_d = tf.placeholder(tf.float32, [None], name='labeled_value')
+        W = tf.placeholder(tf.int32, [None], name='win_status')
+        enqueue_op = data_queue.enqueue_many([X, tf.expand_dims(L_d, axis=1),
+            tf.expand_dims(W, axis=1)])
+        B, L, b_W = data_queue.dequeue_many(args.batch_size)
 
     converted_board = tf.one_hot(B, 13)
 
@@ -235,12 +301,16 @@ def main():
                 tf.nn.sigmoid_cross_entropy_with_logits(logits=value, labels=L))
 
         abs_error = tf.reduce_mean(tf.abs(value - (L * 200 - 100)))
+        mse_error = tf.reduce_mean(tf.squared_difference(value, L))
         regularizer = tf.add_n([tf.nn.l2_loss(v) for v in
             tf.trainable_variables() if not "bias" in v.name]) * 0.001
-        error = entropy_error + regularizer
+        error = mse_error
         tf.summary.scalar("Error", error)
+        tf.summary.histogram("Target Values", L)
 
-        accuracy = tf.reduce_mean(tf.cast(tf.greater(tf.sign(value) * tf.sign(L - 0.5), 0),
+        accuracy = tf.reduce_mean(tf.cast(
+                tf.greater(
+                    tf.sign(value) * tf.sign(tf.cast(b_W, tf.float32)), 0),
                 tf.float32))
         tf.summary.scalar("Accuracy", accuracy)
 
@@ -253,7 +323,7 @@ def main():
             delay = tf.Variable(MAX_DELAY, trainable=False)
             lr = tf.Variable(0.003, trainable=False)
             tf.summary.scalar("Learning_rate", lr)
-            opt = tf.train.GradientDescentOptimizer(lr)
+            opt = tf.train.AdamOptimizer(1e-4)
             gvs = opt.compute_gradients(error)
             tf.summary.scalar('gradient_norm', tf.global_norm(list(zip(*gvs))[0]))
 
@@ -302,11 +372,12 @@ def main():
 
         t = []
         t = [threading.Thread(target=play_self,
-            args=(sess, enqueue_op, X, L_d, v, coord),
-            kwargs={'engine': load_engine()}) for v in q_vs[:-1]]
+            args=(sess, enqueue_op, X, L_d, v, coord, W),
+            kwargs={'engine': load_engine(), 'thread_id': i, 'sw': writer})
+            for i, v in enumerate(q_vs[:-1])]
         t.append(threading.Thread(target=play_self, args=(sess, enqueue_op, X,
-            L_d, q_vs[-1], coord), kwargs={"log_dir": "chesslogs", "engine":
-                load_engine()}))
+            L_d, q_vs[-1], coord, W), kwargs={"log_dir": "chesslogs", "engine":
+                load_engine(), 'sw': writer}))
         # t.append(threading.Thread(target=data_loading_thread, args=(sess,
             # enqueue_op, X, L_d, coord)))
         train_thread = threading.Thread(target=train,
@@ -429,12 +500,73 @@ def load_data(data_dir=None):
                 game = read_game(handler)
 
 
-def load_engine(location="./stockfish_8_x64"):
+def load_engine(location=None):
     from chess import uci
+    if location is None:
+        if os.name == 'posix':
+            location = './stockfish_8_x64'
+        if os.name == 'nt':
+            location = 'stockfish_8_x64.exe'
+    assert(location is not None)
     engine = uci.popen_engine(location)
     engine.uci()
 
     return engine
+
+
+def naiveminmax(board, depth, getbestmove=False):
+    assert(depth <= 5)
+    if depth == 0:
+        assert(not getbestmove)
+        if len(board.legal_moves) != 0:
+            return random.uniform(-10, 10)
+        return -10
+
+    best_score = -10
+    best_move = None
+    for mv in board.legal_moves:
+        board.push(mv)
+        new_score = -naiveminmax(board, depth - 1)
+        board.pop()
+        if new_score > best_score:
+            best_score = new_score
+            best_move = mv
+
+    if getbestmove:
+        return best_move
+    else:
+        return best_score
+
+
+def test_naive_minmax(filename="chesslogs/naiveminmax.pgn"):
+    from chess import pgn
+    board = chess.Board()
+    mv = naiveminmax(board, 2, True)
+    while mv is not None and not board.is_game_over():
+        board.push(mv)
+        mv = naiveminmax(board, 2, True)
+        if mv is None:
+            break
+        print("Moved: ", mv.uci())
+    print("Game status:", board.result())
+    f = open(filename, 'w')
+    print(pgn.Game.from_board(board), file=f)
+
+
+def run_uci_mode():
+    if input() != 'uci':
+        exit()
+    board = chess.Board()
+    while True:
+        cmd = input()
+        args = cmd.split(' ')
+        if args[0] == 'isready':
+            print('readyok')
+        if args[0] == 'setoption':
+            pass
+        if args[0] == 'ucinewgame':
+            pass
+    pass
 
 
 if __name__ == '__main__':
